4db725cb1740b7720f0afd0a9b7d62215ff9dab3
root <contact@pacomeperrichot.com>
2021-10-13 09:17:13 +0200

    Meilleur indentation index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Météo</title>
@@ -8,7 +9,9 @@
 	<link rel="stylesheet" href="assets/css/preloader.csss" />
 	<link rel="stylesheet" href="assets/css/style.css" />
 	<script src='assets/js/script.js'></script>
+
 </head>
+
 <body>
 
 	<div class="preloader">
@@ -35,96 +38,97 @@
 
 	<div class="wrapper">
 		<section class="bordure">
-		    <p>Météo</p>
+			<p>Météo</p>
 		</section>
 		<section class="contenu">
-		    <h1>
-		    	Limoges, FR 
-		    	<a href="http://maps.google.com/maps?q=45.831532,1.2578 class="lk" target="_blank" title="Voir sur une carte">
-		    		Voir sur une carte
-		    	</a>
-		    </h1>
-		    <div class="jour">
-	    		<div class="numero_jour">
-	    			<h2>Météo du 07/10/2019</h2>
-	    		</div>
-			    <div class="temperature entypo-light-up">
-			    	<h2>
-			    		16.06<span class="degree-symbol">°</span>C
-			    	</h2>
-			    </div>
-			    <ul>
-			      <li class="fontawesome-leaf left">
-			        <span>2.07 km/h</span>
-			      </li>
-			      <li class="fontawesome-tint center">
-			        <span>55%</span>
-			      </li>
-			      <li class="fontawesome-dashboard right">
-			        <span>995.27</span>
-			      </li>
-			    </ul> 
-			    <div class="description">
-			    	Description : Peu nuageux
-			    </div>
-			</div>
-		    <div class="jour">
-	    		<div class="numero_jour">
-	    			<h2>Météo du 08/10/2019</h2>
-	    		</div>
-			    <div class="temperature entypo-light-up">
-				    <h2>
-						15.72<span class="degree-symbol">°</span>C
-				    </h2>
-			    </div>
-			    <ul>
-			      <li class="fontawesome-leaf left">
-			        <span>4.26 km/h</span>
-			      </li>
-			      <li class="fontawesome-tint center">
-			        <span>58%</span>
-			      </li>
-			      <li class="fontawesome-dashboard right">
-			        <span>999.46</span>
-			      </li>
-			    </ul> 
-			    <div class="description">
-			    	Description : Ensoleillé
-			    </div>
-			</div>
-		    <div class="jour">
-	    		<div class="numero_jour">
-	    			<h2>Météo du 09/10/2019</h2>
-	    		</div>
-
-			    <div class="temperature entypo-light-up">
+			<h1>
+				Limoges, FR
+				<a href="http://maps.google.com/maps?q=45.831532,1.2578 class=" lk" target="_blank" title="Voir sur une carte">
+					Voir sur une carte
+				</a>
+			</h1>
+			<div class="jour">
+				<div class="numero_jour">
+					<h2>Météo du 07/10/2019</h2>
+				</div>
+				<div class="temperature entypo-light-up">
 					<h2>
-			      		14.69<span class="degree-symbol">°</span>C
-			      	</h2>
-			    </div>
-			    <ul>
-			      <li class="fontawesome-leaf left">
-			        <span>3.61 km/h</span>
-			      </li>
-			      <li class="fontawesome-tint center">
-			        <span>57%</span>
-			      </li>
-			      <li class="fontawesome-dashboard right">
-			        <span>1002.7</span>
-			      </li>
-			    </ul> 
-			    <div class="description">
-			    	Description : Ensoleillé
-			    </div>
+						16.06<span class="degree-symbol">°</span>C
+					</h2>
+				</div>
+				<ul>
+					<li class="fontawesome-leaf left">
+						<span>2.07 km/h</span>
+					</li>
+					<li class="fontawesome-tint center">
+						<span>55%</span>
+					</li>
+					<li class="fontawesome-dashboard right">
+						<span>995.27</span>
+					</li>
+				</ul>
+				<div class="description">
+					Description : Peu nuageux
+				</div>
 			</div>
-		    <div class="bullets">
-	    		<span class="entypo-record" data-cible="0"></span>
-	    		<span class="entypo-record" data-cible="1"></span>
-	    		<span class="entypo-record" data-cible="2"></span>
-		    </div>
+			<div class="jour">
+				<div class="numero_jour">
+					<h2>Météo du 08/10/2019</h2>
+				</div>
+				<div class="temperature entypo-light-up">
+					<h2>
+						15.72<span class="degree-symbol">°</span>C
+					</h2>
+				</div>
+				<ul>
+					<li class="fontawesome-leaf left">
+						<span>4.26 km/h</span>
+					</li>
+					<li class="fontawesome-tint center">
+						<span>58%</span>
+					</li>
+					<li class="fontawesome-dashboard right">
+						<span>999.46</span>
+					</li>
+				</ul>
+				<div class="description">
+					Description : Ensoleillé
+				</div>
+			</div>
+			<div class="jour">
+				<div class="numero_jour">
+					<h2>Météo du 09/10/2019</h2>
+				</div>
+
+				<div class="temperature entypo-light-up">
+					<h2>
+						14.69<span class="degree-symbol">°</span>C
+					</h2>
+				</div>
+				<ul>
+					<li class="fontawesome-leaf left">
+						<span>3.61 km/h</span>
+					</li>
+					<li class="fontawesome-tint center">
+						<span>57%</span>
+					</li>
+					<li class="fontawesome-dashboard right">
+						<span>1002.7</span>
+					</li>
+				</ul>
+				<div class="description">
+					Description : Ensoleillé
+				</div>
+			</div>
+			<div class="bullets">
+				<span class="entypo-record" data-cible="0"></span>
+				<span class="entypo-record" data-cible="1"></span>
+				<span class="entypo-record" data-cible="2"></span>
+			</div>
 		</section>
 	</div>
 	<script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
-	
+
 </body>
-</html>   
+
+</html>
